24eafa5b12b169fd27a82bde37bc4b8a5adbc8ad
David Gras Crespo <davidgcre@CERMACLTDAVIDG.home>
2022-08-07 20:17:37 +0200

    Add a TodoItem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,7 @@
+import Todo from "../models/todos";
+
+const TodoItem: React.FC<{ text: string }> = (props) => {
+  return <li>{props.text}</li>;
+};
+
+export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import Todo from "../models/todos";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
