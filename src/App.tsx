953d1258ca325d553e52be5a1555d16474509caa
David Gras Crespo <davidgcre@CERMACLTDAVIDG.home>
2022-08-07 22:05:56 +0200

    Working with "function props" and managing State with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todos";
 
 function App() {
-  const todos: Todo[] = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // new Todo("Learn React"), new Todo("Learn TypeScript")
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);    
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+  }
+  
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -11,6 +11,8 @@ const NewTodo = () => {
     if (enteredText.trim().length === 0) {
       return;
     }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
